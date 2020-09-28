125c316284767ee105d325224c0b3f49257bd056
robincorey <robin.corey@gmail.com>
2020-09-28 12:53:24 +0100

    bug fixed in leaflet script

--- PLOT_leaflets.py
+++ PLOT_leaflets.py
@@ -1,4 +1,3 @@
-#!/sansom/s137/bioc1535/programs/anaconda2/bin/python
 import scipy as sc
 import numpy as np
 from scipy import stats
@@ -13,9 +12,8 @@ from matplotlib.lines import Line2D
 
 def get_total(pdb,leaflet):
 	all_lipids = []
-	lipid_total = []
-	print pdb
 	for lipid in lipids:
+		lipid_total = [] # moved to here!
 		for num in ['1','2','3','4','5']:
 				ydata = np.loadtxt(fname='Leaflets/%s/%s/%s.%s.xvg' % ( leaflet,pdb,lipid,num ), usecols=(1), comments=['@','#'], unpack=True)
 				lipid_total.append(np.mean(ydata))
@@ -53,7 +51,7 @@ def ttest(up, low, x):
 
 leaflets = [ 'lower', 'upper' ]
 lipids = [ 'POPE' , 'POPG', 'CARD' ]
-pdbs = [ '5OQT' ,'1KQF','2IC8','5ZUG','4JR9','3O7P','1NEK','3ZE3','5MRW','5OC0','1ZCD','1PV6','3OB6','4IU8','1KF6','3QE7','1KPK','5SV0','1U77','1FFT','5AJI','4ZP0','5AZC','1Q16','1FX8','6AL2', '2QFI','3DHW','1IWG','3K07','4GD3','5JWY','3B5D','4Q65','1PW4','2R6G','4DJI','2WSX','1L7V','1RC2']
+pdbs = [ '5OQT'  ,'1KQF','2IC8','5ZUG','4JR9','3O7P','1NEK','3ZE3','5MRW','5OC0','1ZCD','1PV6','3OB6','4IU8','1KF6','3QE7','1KPK','5SV0','1U77','1FFT','5AJI','4ZP0','5AZC','1Q16','1FX8','6AL2', '2QFI','3DHW','1IWG','3K07','4GD3','5JWY','3B5D','4Q65','1PW4','2R6G','4DJI','2WSX','1L7V','1RC2']
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
@@ -74,16 +72,16 @@ for counter, lipid in enumerate(lipids):
 	p2 = ttest(up,low,counter)
 	print p2
 	
-#plt.plot([-1,3.175],[67,67],color='orange', linestyle='dashed',linewidth=1,alpha=0.5)
-#plt.plot([-1,3.175],[23,23],color='red', linestyle='dashed',linewidth=1,alpha=0.5)
-#plt.plot([-1,3.175],[10,10],color='royalblue',linestyle='dashed', linewidth=1,alpha=0.5)
+plt.plot([-1,3.175],[67,67],color='orange', linestyle='dashed',linewidth=1,alpha=0.5)
+plt.plot([-1,3.175],[23,23],color='red', linestyle='dashed',linewidth=1,alpha=0.5)
+plt.plot([-1,3.175],[10,10],color='royalblue',linestyle='dashed', linewidth=1,alpha=0.5)
 
 plt.ylabel('lipids bound (%)', fontsize=25 )
-plt.yticks(np.arange(0, 51, step=10),fontsize=20)
+plt.yticks(np.arange(0, 71, step=10),fontsize=20)
 plt.xticks((-0.175,0.175,0.825,1.175,1.825,2.175), ('inner', 'outer', 'inner', 'outer', 'inner', 'outer'), rotation=45, fontsize=20)
 plt.xlim([-0.5,2.5])
 custom_lines = [Line2D([0], [0], color='orange', lw=2),
                 Line2D([0], [0], color='red', lw=2),
                 Line2D([0], [0], color='royalblue', lw=2)]
-plt.legend(custom_lines, ['POPE','POPG','CDL'], fontsize=15,frameon=False)
+plt.legend(custom_lines, ['POPE','POPG','CDL'], fontsize=15,frameon=False, loc='upper right')
 plt.savefig('Figs/Leaflets.png',bbox_inches='tight', dpi=900)
